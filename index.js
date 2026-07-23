1e5876a9a4e03eca41af66841795b1e0d9a81a47
Mugabe Ben Toussaint <mbentoussaint@gmail.com>
2026-07-23 15:46:06 +0200

    codewars: Build Tower

--- index.js
+++ index.js
@@ -601,3 +601,46 @@ function order(words){
   }
   return output.join(' ');
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/*Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]*/
+//solution.
+function towerBuilder(nFloors) {
+  // build here
+  let final=[];
+  const baseLength=(nFloors*2)-1;
+  for(let i=1;i<=nFloors;i++){
+      let output=[];
+    for(let j=0;j<baseLength;j++){
+      let repeater=1;
+      if(j >= nFloors - i && j < nFloors + i - 1){
+        output.push('*')
+      }
+      else{
+        output.push(' ')
+      }
+        repeater+=2;
+    }
+    final.push(output.join(''));
+  }
+  return final
+}
+console.log(towerBuilder(3))
