1fe9c8c68c3bb97bfd4cac184b2c20e84257f8c1
Mugabe Ben Toussaint <mbentoussaint@gmail.com>
2026-08-24 14:23:00 +0200

    codewars on String ends with?

--- index.js
+++ index.js
@@ -896,3 +896,18 @@ function alphabetPosition(text) {
   }
   return output.join(" ");
 }
+////////////////////////////////////////////////////////////
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+Inputs: "abc", "bc"
+Output: true
+
+Inputs: "abc", "d"
+Output: false*/
+//solutions
+function solution(str, ending){
+  return str.endsWith(ending);
+}
