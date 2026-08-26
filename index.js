d931007a478fcd8a3d8c42b5f45a8e3a3518ff59
Mugabe Ben Toussaint <mbentoussaint@gmail.com>
2026-08-26 14:46:17 +0200

    codewars on Square Every Digit

--- index.js
+++ index.js
@@ -1021,3 +1021,17 @@ function smash (words) {
    }
   return Words.join(' ');
 };
+//////////////////////////////////////////////////////////////////////////////////////////////
+/*Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Note: The function accepts a non-negative integer and returns an integer.
+
+Happy Coding!*/
+//solution
+function squareDigits(num){
+  return Number(num.toString().split('').map((num)=>Number(num)*num).join(''));
+}
