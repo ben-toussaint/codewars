3e5af6f65e1cec64775d2320985bf8847f62c842
Mugabe Ben Toussaint <mbentoussaint@gmail.com>
2026-09-09 12:41:42 +0200

    codewaars on Count the smiley faces!

--- index.js
+++ index.js
@@ -1332,3 +1332,48 @@ var uniqueInOrder = function(iterable) {
   
   return result;
 };
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+Note
+In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.*/
+//solution
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+  if(arr.length===0)return 0;
+  const validEyes=[';',':'];
+  const validNoses=['-','~'];
+  const validMouths=[')','D'];
+  let counter=0;
+  for(let face of arr){
+    if(face.length===2){
+      const [eyes,mouth]=face.split('');
+      if(validEyes.includes(eyes) && validMouths.includes(mouth)){
+        counter++
+      }
+    }
+    else if(face.length===3){
+      const [eyes,nose,mouth]=face.split('');
+      if(validEyes.includes(eyes) && validNoses.includes(nose) && validMouths.includes(mouth)){
+        counter++
+      }
+      
+      
+    }
+  }
+  return counter;
+}
