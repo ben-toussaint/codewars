54b984e80c8bc6193aed35becff01faaca2e340e
Mugabe Ben Toussaint <mbentoussaint@gmail.com>
2026-09-15 16:17:15 +0200

    codewars on is Anagram

--- index.js
+++ index.js
@@ -1377,3 +1377,23 @@ function countSmileys(arr) {
   }
   return counter;
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"*/
+// write the function isAnagram
+var isAnagram = function(test, original) {
+  if(test.length==original.length){
+    let formatedArr=test.toUpperCase().split('').sort().join('');
+    let originalFormated=original.toUpperCase().split('').sort().join('');
+    return formatedArr==originalFormated?true:false;
+  }
+  return false;
+}
