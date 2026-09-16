9dc187d654ecc2d72f576aac1c2dd6c9b617225b
Mugabe Ben Toussaint <mbentoussaint@gmail.com>
2026-09-16 12:23:04 +0200

    Maximum SubArray sum using kadane's algorithm

--- index.js
+++ index.js
@@ -1397,3 +1397,29 @@ var isAnagram = function(test, original) {
   }
   return false;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+For example:
+
+Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+Output: 6 (Sum of [4, -1, 2, 1])
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead. Your solution should be fast, it will be tested on very large arrays so slow solutions will time out.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+*/
+//solution
+//using kadane's algorithm
+var maxSequence = function(arr){
+  // ...
+  if(arr.length==0)return 0;
+  let currentSum=0;
+  let maxSum=0;
+  for(let i=0;i<arr.length;i++){
+    currentSum+=arr[i];
+    if(currentSum<0){currentSum=0;}
+    if(currentSum>maxSum){maxSum=currentSum;}
+  }
+  return maxSum;
+}
