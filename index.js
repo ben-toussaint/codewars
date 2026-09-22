a62d55316dda185a24ed9c395be188c56fd2cc0e
Mugabe Ben Toussaint <mbentoussaint@gmail.com>
2026-09-22 14:00:05 +0200

    String Incrementer

--- index.js
+++ index.js
@@ -1435,3 +1435,36 @@ function domainName(url){
   const final=cleanUrl.slice(0,cleanUrl.indexOf('.'));
   return final
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/*Your job is to write a function which increments a string, to create a new string.
+
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+
+foo -> foo1
+
+foobar23 -> foobar24
+
+foo0042 -> foo0043
+
+foo9 -> foo10
+
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.*/
+//solution
+function incrementString(strng) {
+    const match = strng.match(/^(.*?)(\d*)$/);
+    const prefix = match[1];
+    const numberStr = match[2];
+
+    if (!numberStr) {
+        return prefix + '1';
+    }
+
+    const incrementedNum = Number(numberStr) + 1;
+
+    const paddedNumber = String(incrementedNum).padStart(numberStr.length, '0');
+    return prefix + paddedNumber;
+}
